a45dcf0fb6824d7bf959d45b33c3f7bb4789d7b9
takumiuma <takutaku915@gmail.com>
2023-12-17 19:51:51 +0900

    テストブランチでのコミット

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
+<script setup>
+import { VBtn,VIcon,VTextField,VResponsive } from 'vuetify/components'
+import { mdiDelete,mdiCheckBold,mdiPlaylistEdit } from '@mdi/js';
+import SvgIcon from '@jamescoyle/vue-icon';
+</script>
 <script>
 export default {
-
   data() {
     return {
       todo:'',
       todos:[],
       completeTodos:[],
       id:1,
+      icons:{
+        mdiCheckBold,
+        mdiDelete,
+        mdiPlaylistEdit,
+      },
+      name: "my-component",
+      components: {
+        SvgIcon
+      },
     }
   },
   computed:{
@@ -34,40 +47,54 @@ export default {
 }
 </script>
 <template>
-  <input type="text" v-model="todo">
-  <button @click="addTodo">追加</button>
-  <div id="todoArea">Todoリスト
-    <ul v-for=" todo in todos" :key="todo.id">
-      <li>
-        {{todo.content}}
-        <button @click="deleteTodo(todo)">削除</button>
-        <button @click="completeTodo(todo)">完了</button>
-      </li>
-    </ul>
-  </div>
-  <div id="completeArea">完了済みのTodo
-    <ul v-for=" completeTodo in completeTodos" :key="completeTodo.id">
-      <li>
-        {{completeTodo.content}}
-        <button @click="returnTodo(completeTodo)">戻る</button>
-      </li>
-    </ul>
-  </div>
+  <v-responsive class="mx-auto" max-width="400">
+    <v-text-field v-model="todo" clearable label="TODOを入力">
+      <template v-slot:append>
+        <v-btn @click="addTodo">
+          <svg-icon type="mdi" :path="icons.mdiPlaylistEdit"></svg-icon>追加
+        </v-btn>
+      </template>
+    </v-text-field>
+  </v-responsive>
+  <v-responsive class="mx-auto" max-width="500">
+    <div id="todoArea">Todoリスト
+      <ul v-for=" todo in todos" :key="todo.id">
+        <li>
+          {{todo.content}}
+          <v-btn @click="deleteTodo(todo)">
+            <svg-icon type="mdi" :path="icons.mdiDelete"></svg-icon>削除
+          </v-btn>
+          <v-btn color="primary" @click="completeTodo(todo)">
+            <v-icon class="ma-2" start icon="mdi-checkbox-marked-circle"></v-icon>完了
+          </v-btn>
+        </li>
+      </ul>
+    </div>
+    <div id="completeArea">完了済みのTodo
+      <ul v-for=" completeTodo in completeTodos" :key="completeTodo.id">
+        <li>
+          {{completeTodo.content}}
+          <v-btn color="orange-darken-2" @click="returnTodo(completeTodo)">
+            <v-icon start icon="mdi-arrow-left"></v-icon>戻す
+          </v-btn>
+        </li>
+      </ul>
+    </div>
+  </v-responsive>
 </template>
 
 <style>
   #todoArea{
-    width: 400px;
-    height: 200px;
     background-color: gray;
     padding: 4px 16px;
     color:black;
   }
   #completeArea{
-    width: 400px;
-    height: 200px;
     background-color: aliceblue;
     padding: 4px 16px;
     color:black;
+  }
+  ul {
+  list-style: none;
   }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import './assets/main.css'
+import "vuetify/dist/vuetify.min.css";
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify';
 
 import App from './App.vue'
 import router from './router'
 
+
+
+
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createVuetify())
 app.use(router)
 
 app.mount('#app')
