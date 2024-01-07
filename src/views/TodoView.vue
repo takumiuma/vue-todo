b0c735fbe4156196d35ecc8dd17b7ce02b736c60
takumiuma <takutaku915@gmail.com>
2024-01-08 00:55:37 +0900

    ボタンの位置を修正

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -31,10 +31,10 @@ export default {
   },
   computed: {},
   methods: {
-    //バリデーション
+    // バリデーション
     required: (value) => !!value || '必ず入力してください',
     limit_length: (value) => value.length <= 200 || '200文字以内で入力してください',
-    //todo追加関数
+    // todo追加関数
     async addTodo() {
       const { valid } = await this.$refs.form.validate()
       if (!valid) return
@@ -49,23 +49,23 @@ export default {
       })
       this.$refs.form.reset()
     },
-    //todoの削除（１つ）
+    // todoの削除（１つ）
     deleteTodo(todo) {
       const index = this.todos.indexOf(todo)
       this.todos.splice(index, 1)
     },
-    //todoを完了テーブルに移動（１つ）
+    // todoを完了テーブルに移動（１つ）
     completeTodo(todo) {
       this.completeTodos.push(todo)
       this.deleteTodo(todo)
     },
-    //todoを仕掛かりテーブルに移動（１つ）
+    // todoを仕掛かりテーブルに移動（１つ）
     returnTodo(todo) {
       this.todos.push(todo)
       const index = this.completeTodos.indexOf(todo)
       this.completeTodos.splice(index, 1)
     },
-    //指定したテーブルのtodos,またはcompleteTodosを全て削除
+    // 指定したテーブルのtodos,またはcompleteTodosを全て削除
     clearTodo(emptyTodos, target) {
       if (target === 'todos') {
         this.todos = emptyTodos
@@ -73,7 +73,7 @@ export default {
         this.completeTodos = emptyTodos
       }
     },
-    //指定したテーブルのtodos,またはcompleteTodosを全て入れ替え
+    // 指定したテーブルのtodos,またはcompleteTodosを全て入れ替え
     moveTodo(list, target) {
       if (target === 'todos') {
         this.completeTodos.push(...list)
@@ -83,11 +83,11 @@ export default {
         this.completeTodos.splice(0)
       }
     },
-    //現在時刻を取得
+    // 現在時刻を取得
     getTime() {
       return dayjs().format('YYYY-MM-DD HH:mm')
     },
-    //itemごとのbool値プロパティを切り替え、編集用のタグを表示
+    // itemごとのbool値プロパティを切り替え、編集用のタグを表示
     editStart(editTarget, todo) {
       const index = this.todos.indexOf(todo)
       if (editTarget === 'todo') {
@@ -135,89 +135,85 @@ export default {
         </v-row>
       </v-form>
     </v-container>
-    <v-responsive class="mx-auto">
-      <TodoTableTitle
-        title="In progress..."
-        :todoList="todos"
-        @clear-click="clearTodo($event, 'todos')"
-        @move-click="moveTodo($event, 'todos')"
-      />
-      <v-data-table :headers="headers" :items="todos" items-per-page="5">
-        <template v-slot:item="{ item }">
-          <tr>
-            <td>{{ item.time }}</td>
-            <td>
-              <span v-if="item.onEditTodo">
-                <v-form ref="todoEdit">
-                  <v-row>
-                    <v-col cols="9">
-                      <v-text-field
-                        v-model="item.content"
-                        :rules="[required, limit_length]"
-                        counter="200"
-                        clearable
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="3">
-                      <v-btn @click="editFinish('todo', item)">編集完了</v-btn>
-                    </v-col>
-                  </v-row>
-                </v-form>
-              </span>
-              <span v-else>
-                {{ item.content }}
-                <v-btn @click="editStart('todo', item)">編集</v-btn>
-              </span>
-            </td>
-            <td>
-              <span v-if="item.onEditPerson">
-                <v-row>
-                  <v-col>
-                    <v-select :rules="[required]" v-model="item.pic" :items="pics"></v-select>
+    <TodoTableTitle
+      title="In progress..."
+      :todoList="todos"
+      @clear-click="clearTodo($event, 'todos')"
+      @move-click="moveTodo($event, 'todos')"
+    />
+    <v-data-table :headers="headers" :items="todos" items-per-page="5">
+      <template v-slot:item="{ item }">
+        <tr>
+          <td>{{ item.time }}</td>
+          <td>
+            <span v-if="item.onEditTodo">
+              <v-form ref="todoEdit">
+                <v-row class="align-center">
+                  <v-col cols="9">
+                    <v-text-field
+                      v-model="item.content"
+                      :rules="[required, limit_length]"
+                      counter="200"
+                      clearable
+                    ></v-text-field>
                   </v-col>
-                  <v-col>
-                    <v-btn @click="editFinish('pic', item)">編集完了</v-btn>
+                  <v-col cols="3">
+                    <v-btn @click="editFinish('todo', item)">編集完了</v-btn>
                   </v-col>
                 </v-row>
-              </span>
-              <span v-else>
-                {{ item.pic }}
-                <v-btn @click="editStart('pic', item)">編集</v-btn>
-              </span>
-            </td>
-            <td>
-              <v-btn @click="deleteTodo(item)">
-                <svg-icon type="mdi" :path="icons.mdiDelete"></svg-icon>削除
-              </v-btn>
-              <v-btn color="green-darken-1" @click="completeTodo(item)">
-                <v-icon class="ma-2" start icon="mdi-checkbox-marked-circle"></v-icon>完了
-              </v-btn>
-            </td>
-          </tr>
-        </template>
-      </v-data-table>
-    </v-responsive>
-    <v-responsive class="mx-auto">
-      <TodoTableTitle
-        title="Done!!"
-        :todoList="completeTodos"
-        @clear-click="clearTodo($event, 'completeTodos')"
-        @move-click="moveTodo($event, 'completeTodos')"
-      />
-      <v-data-table :headers="headers" :items="completeTodos" items-per-page="5">
-        <template v-slot:item="{ item }">
-          <tr>
-            <td>{{ item.time }}</td>
-            <td>{{ item.content }}</td>
-            <td>{{ item.pic }}</td>
-            <td>
-              <v-btn color="orange-darken-2" @click="returnTodo(item)">
-                <v-icon start icon="mdi-arrow-left"></v-icon>戻す
-              </v-btn>
-            </td>
-          </tr>
-        </template>
-      </v-data-table>
-    </v-responsive>
+              </v-form>
+            </span>
+            <span v-else>
+              {{ item.content }}
+              <v-btn @click="editStart('todo', item)">編集</v-btn>
+            </span>
+          </td>
+          <td>
+            <span v-if="item.onEditPerson">
+              <v-row class="align-center">
+                <v-col>
+                  <v-select :rules="[required]" v-model="item.pic" :items="pics"></v-select>
+                </v-col>
+                <v-col>
+                  <v-btn @click="editFinish('pic', item)">編集完了</v-btn>
+                </v-col>
+              </v-row>
+            </span>
+            <span v-else>
+              {{ item.pic }}
+              <v-btn @click="editStart('pic', item)">編集</v-btn>
+            </span>
+          </td>
+          <td>
+            <v-btn @click="deleteTodo(item)">
+              <svg-icon type="mdi" :path="icons.mdiDelete"></svg-icon>削除
+            </v-btn>
+            <v-btn color="green-darken-1" @click="completeTodo(item)">
+              <v-icon class="ma-2" start icon="mdi-checkbox-marked-circle"></v-icon>完了
+            </v-btn>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+    <TodoTableTitle
+      title="Done!!"
+      :todoList="completeTodos"
+      @clear-click="clearTodo($event, 'completeTodos')"
+      @move-click="moveTodo($event, 'completeTodos')"
+    />
+    <v-data-table :headers="headers" :items="completeTodos" items-per-page="5">
+      <template v-slot:item="{ item }">
+        <tr>
+          <td>{{ item.time }}</td>
+          <td>{{ item.content }}</td>
+          <td>{{ item.pic }}</td>
+          <td>
+            <v-btn color="orange-darken-2" @click="returnTodo(item)">
+              <v-icon start icon="mdi-arrow-left"></v-icon>戻す
+            </v-btn>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
   </div>
 </template>
